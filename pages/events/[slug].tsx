16ce6732bdd8fb6e3ce92d2088414cd56be8f7cf
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2021-12-15 00:25:05 +0100

    Single event page

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -1,12 +1,92 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import { Event } from 'types';
+import styles from '@/styles/Event.module.css';
+import { DeleteEvent } from 'types';
 
-const EventPage: NextPage = () => {
+const EventPage: NextPage<{ evt: Event }> = ({ evt }) => {
+  const deleteEvent: DeleteEvent = () => {
+    console.log('Delete');
+  };
+
   return (
     <Layout>
-      <h1>My Event</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href='#' className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} alt='' />
+          </div>
+        )}
+
+        <h3>Performers: </h3>
+        <p>{evt.performers}</p>
+        <h3>Description</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href='/events'>
+          <a className={styles.back}>{'<'} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 };
 
 export default EventPage;
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt: Event) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
+};
+
+// export const getServerSideProps = async ({ query: { slug } }) => {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   };
+// };
--- types.ts
+++ types.ts
@@ -17,3 +17,5 @@ export interface Event {
   description: string;
   image?: string;
 }
+
+export type DeleteEvent = () => void;

e569a458c940649097c6c68d9851f8a7f29a5860
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2021-12-29 14:55:11 +0100

    Added image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', process.env.IMAGE_DOMAIN],
+    domains: ['localhost', 'dj-events-strapi-backend21.herokuapp.com'],
   },
 };
